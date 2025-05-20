dac9ed6d21513a3ce75ca64d699f21b7a87a75c1
Maximo Julian Roman Rodriguez <134997528+NotDiscordia@users.noreply.github.com>
2025-05-19 18:19:28 -0700

    Cambios de cositas

--- RefresqueriaVicoGestion/src/main/java/ByteBuilders/Controladores/AltaEmpleadoServlet.java
+++ RefresqueriaVicoGestion/src/main/java/ByteBuilders/Controladores/AltaEmpleadoServlet.java
@@ -2,13 +2,14 @@ package ByteBuilders.Controladores;
 
 import ByteBuilders.Entidad.Usuario;
 import ByteBuilders.Negocio.UsuarioService;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AltaEmpleadoServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws IOException {
 
         Usuario empleado = new Usuario();
         empleado.setNombreCompleto(request.getParameter("nombre"));  // ✔️ Método correcto
