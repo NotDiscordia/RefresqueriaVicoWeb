ccd2e64b2bb3257c52e31be826ed245ec1173413
Alan J. Ruiz <144744709+ServBot213@users.noreply.github.com>
2025-06-13 16:02:22 -0700

    HTMLS corregidos, falta corte de caja y funcionalidad en botones

--- RefresqueriaVicoGestion/src/main/java/ByteBuilders/Persistencia/ConexionBD.java
+++ RefresqueriaVicoGestion/src/main/java/ByteBuilders/Persistencia/ConexionBD.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
-    private static final String URL = "jdbc:mysql://localhost:3306/vicodb?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3307/vicodb?useSSL=false&serverTimezone=UTC";
     private static final String USUARIO = "root";
-    private static final String CONTRASENA = "1234";
+    private static final String CONTRASENA = "alan123";
 
     public static Connection obtenerConexion() {
         try {
